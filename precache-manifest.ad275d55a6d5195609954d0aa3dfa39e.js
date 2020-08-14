004e31f723012c557ee7b26803b6ca4d00344e11
Debaprasad-dez <debaprasadpaul033@gmail.com>
2020-08-14 23:09:15 +0530

    Updates

---
+++ precache-manifest.ad275d55a6d5195609954d0aa3dfa39e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e21f0520043f9fc7b161df86d47a20",
+    "url": "/advize/index.html"
+  },
+  {
+    "revision": "86101ada1513b234c836",
+    "url": "/advize/static/css/main.8cad900f.chunk.css"
+  },
+  {
+    "revision": "d07e5e51a2ba44c3955f",
+    "url": "/advize/static/js/2.f105b0a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advize/static/js/2.f105b0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86101ada1513b234c836",
+    "url": "/advize/static/js/main.fa5c8624.chunk.js"
+  },
+  {
+    "revision": "fda80561d63bd4e7a24d",
+    "url": "/advize/static/js/runtime-main.177ad4e1.js"
+  }
+]);
